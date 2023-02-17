f17876ef85fa48926f45a641696927606552b6f4
karianmash <ianmachariak17@gmail.com>
2023-02-18 00:25:44 +0300

    Looked at math functions

---
+++ 5-math-functions.sql
@@ -0,0 +1,16 @@
+-- (B) MATH FUNCTIONS
+-- ------------------------------------------------------------
+-- (1) Function to get the absolute value of a number
+--
+-- It returns the value range from -9223372036854775808 to 9223372036854775807
+SELECT
+    ABS(-1);
+
+-- ------------------------------------------------------------
+-- (2) Function to get the arc cosine of a number
+--
+-- It returns the value range from -1 to 1
+SELECT
+    ACOS(1);
+
+-- ------------------------------------------------------------
