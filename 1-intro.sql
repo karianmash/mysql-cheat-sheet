5fc0bc98d86e7166668d6902d2807fd467b25ade
karianmash <ianmachariak17@gmail.com>
2023-02-17 16:27:59 +0300

    Create and use db query

--- 1-intro.sql
+++ 1-intro.sql
@@ -1,2 +1,9 @@
 -- Introduction to MySQL
+-- -------------------------------------------------------------------------------------------------
 
+-- (a) Create a database called 'mydb' and use it.
+CREATE DATABASE mydb;
+USE mydb;
+-- -------------------------------------------------------------------------------------------------
+
+-- (b) Create a table called 'mytable' with the following fields:
