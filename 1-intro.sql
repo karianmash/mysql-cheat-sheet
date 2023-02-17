23e90efc1a1be781254e2829f6c191185edadc66
karianmash <ianmachariak17@gmail.com>
2023-02-17 16:29:18 +0300

    Create table query

--- 1-intro.sql
+++ 1-intro.sql
@@ -7,3 +7,15 @@ USE mydb;
 -- -------------------------------------------------------------------------------------------------
 
 -- (b) Create a table called 'mytable' with the following fields:
+--     id (int, primary key, auto increment)
+--     name (varchar(50))
+--     age (int)
+--     address (varchar(100))
+--     salary (float)
+CREATE TABLE mytable (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age INT,
+    address VARCHAR(100),
+    salary FLOAT
+);
