e0582377ff25006d3dde5f588ac27ceab00ab1de
karianmash <ianmachariak17@gmail.com>
2023-02-18 00:27:15 +0300

    Looked at math functions

--- 5-math-functions.sql
+++ 5-math-functions.sql
@@ -14,3 +14,29 @@ SELECT
     ACOS(1);
 
 -- ------------------------------------------------------------
+-- (3) Function to get the arc sine of a number
+--
+-- It returns the value range from -1 to 1
+SELECT
+    ASIN(1);
+
+-- ------------------------------------------------------------
+-- (4) Function to get the arc tangent of a number
+--
+-- It returns the value range from -1 to 1
+SELECT
+    ATAN(1);
+
+-- ------------------------------------------------------------
+-- (5) Function to get the arc tangent of two numbers
+--
+-- It returns the value range from -1 to 1
+SELECT
+    ATAN2(1, 1);
+
+-- ------------------------------------------------------------
+-- (6) Function to get the smallest integer value greater than or equal to a number
+--
+-- It returns the value range from -9223372036854775808 to 9223372036854775807
+SELECT
+    CEIL(1.1);
